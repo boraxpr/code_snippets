945f6bec917f6c6d43bfc19c83e091f6643cb4bc
boraxpr <naipawat.poo@student.mahidol.ac.th>
2020-07-16 04:36:37 +0700

    Add an input to the function to improve reusability

--- randomized_name.py
+++ randomized_name.py
@@ -1,4 +1,4 @@
-def get_random_name():
-  randomized_number = np.random.permutation(100)
+def get_random_name(num):
+  randomized_number = np.random.permutation(num)
   str_randomized_number = f'{randomized_number[0]+1:06}'
   return "2018_" + str_randomized_number + ".jpg", "2018_" + str_randomized_number + "new" + ".jpg"
